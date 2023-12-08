f9e29c54f02cb73b5bdd4ebdea5ada90f56191f6
singh-that-codes <500091028@stu.upes.ac.in>
2023-12-08 23:01:49 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xpense_web_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/singhthatcodes/xpense_web_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.singhthatcodes.xpense_web_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
